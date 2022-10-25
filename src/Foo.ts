def3d2bca1f6a787f7a96af767f5fd7ba9a04a87
wenruiLi <36104364+wenruiLiA15@users.noreply.github.com>
2022-10-25 10:48:19 +0800

    feat: add evennumbergreaterthen10

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,3 +3,22 @@ export class Foo {
     return 'bar'
   }
 }
+export function isCuteNumberGreaterThan10(numbers:number[]): boolean {
+
+  const evennumbers = findEvenNumber(numbers)
+  const maxnumber = findMaxNumber(evennumbers)
+
+  return isCreaterThan10(maxnumber)
+}
+function findEvenNumber(numbers: number[]): number[] {
+  return numbers.filter(number => number%2 === 0)
+}
+
+function findMaxNumber(numbers: number[]): number{
+  return Math.max(...numbers)
+}
+
+function isCreaterThan10(number:number): boolean {
+  return number>10
+}
+
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,17 @@
-import {Foo} from '../src/Foo'
+import {Foo,isCuteNumberGreaterThan10} from '../src/Foo'
 
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
   })
+  it('should return ture when input 1,3,4,5,12', () => {
+    // given
+    const numbers = [1,3,4,5,12]
+
+    // when
+    const actual: boolean = isCuteNumberGreaterThan10(numbers)
+    // then
+    expect(actual).toBeTruthy()
+  });
 })
+
